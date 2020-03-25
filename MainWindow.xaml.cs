0a5c880c9229ecf4b8d84747b110b2e0128ea077
39340 <39340@PC-MARIUSZ>
2020-03-25 15:25:03 +0100

    Commit cumulativo.... (precedenti fatti erroneamente)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DateToWeek
 {
@@ -20,22 +11,31 @@ namespace DateToWeek
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Private Variables
         int week = 0;
         int year = 0;
 
+        // regulates the function of automatic parsing.
+        bool isAutomaticParseChecked = true;
+
         DateTime date = DateTime.MinValue;
-        
+        #endregion
+
+        #region Constructor
         public MainWindow()
         {
             InitializeComponent();
         }
+        #endregion
 
+        #region WeekTextbox checks
         private void WeekTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             try
             {
                 week = Int32.Parse(this.WeekTextBox.Text);
-            } catch (FormatException)
+            }
+            catch (FormatException)
             {
                 WeekTextBox.Text = "1";
             }
@@ -56,9 +56,11 @@ namespace DateToWeek
             }
             date = new DateTime(year, 1, 1);
             date = date.AddDays((7 * week) - 7);
-            DateTextbox.Text = date.Date.ToString();
+            DateTextbox.Text = date.Date.ToString("dd.MM.yyyy");
         }
+        #endregion
 
+        #region YearTextbox checks
         private void YearTextbox_TextChanged(object sender, TextChangedEventArgs e)
         {
             try
@@ -70,9 +72,11 @@ namespace DateToWeek
                 YearTextbox.Text = "2000";
             }
 
-            if (year < 1950 || year > 2100)
+            // Cannot accept negative, empty values or higher than 9999
+            if (year <= 0 || year > 9999)
             {
                 YearTextbox.BorderBrush = Brushes.Red;
+                year = 1;
             }
             else
             {
@@ -87,9 +91,113 @@ namespace DateToWeek
                 return;
             }
 
-            DateTextbox.Text = date.Date.ToString();
+            DateTextbox.Text = date.Date.ToString("dd.MM.yyyy");
+        }
+        #endregion
+
+        #region SerialTextBox method
+        private void SerialTextbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Retrives the text from the textbox
+            string text = ((TextBox)sender).Text;
+
+            // initialize the array of splitted results
+            string[] splitted = { };
+
+            // Checks if the text contains a slash
+            if (text.Contains("/") && text.Length >= 8)
+            {
+                // Splits it on the slash (/) character
+                splitted = text.Split("/");
+                SerialResultTextbox.Text = DateOrderReverse(splitted[1]);
+            }
+
+            if (text.Contains("/"))
+            {
+                // Formats the data in an alternative way.
+                splitted = text.Split("/");
+                try
+                {
+                    AlternativeSerialTextbox.Text = $"C-{splitted[1]}";
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    // This exception can be ignored, because it'll be cause everytime.
+                }
+            }
+
+
+
+        }
+        #endregion
+
+        #region Utility methods
+        /// <summary>
+        /// Receive a textbox as param and copy the text to the clipboard
+        /// </summary>
+        /// <param name="sender"></param>
+        private void copyToClipboard(object sender)
+        {
+            Clipboard.SetText(((TextBox)sender).Text);
         }
 
- 
+        /// <summary>
+        /// Receives a text within a date inside, for example 190212 and turnes it to 120219
+        /// </summary>
+        /// <param name="rawData"></param>
+        /// <returns></returns>
+        private string DateOrderReverse(string rawData)
+        {
+            var possibleDate = rawData.Substring(0, 6);
+            string[] date = { "", "", "" };
+
+            date[0] = possibleDate.Substring(4, 2);
+            date[1] = possibleDate.Substring(2, 2);
+            date[2] = possibleDate.Substring(0, 2);
+
+            return string.Join(".", date);
+
+        }
+
+        #endregion
+
+        #region Button behavior
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            copyToClipboard(DateTextbox);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            copyToClipboard(SerialResultTextbox);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            copyToClipboard(AlternativeSerialTextbox);
+        }
+        #endregion
+
+        #region Automatic parsing logic
+        // If automaticPaste is enabled, this textbox will get automatically the data from the clipboard
+        private void SerialTextbox_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (isAutomaticParseChecked)
+            {
+                SerialTextbox.Text = Clipboard.GetText();
+            }
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            isAutomaticParseChecked = true;
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            isAutomaticParseChecked = false;
+        }
+
+        #endregion
     }
 }
